5f9e786680b2c3504becd850c8ca5159c6d55358
boggs <hello@boggs.xyz>
2017-08-22 21:08:47 +0800

    Add commander debugger

--- lib/ind_flow/commander.rb
+++ lib/ind_flow/commander.rb
@@ -1,8 +1,10 @@
 module IndFlow
   class Commander
     def self.send_command(command)
+      puts "DEBUG: Running command **#{command}**..."
       output = `#{command}`
       exit_status = $?.exitstatus
+      puts "DEBUG: Command **#{command}** returned #{exit_status}."
       {
         output: output,
         exit_status: exit_status
