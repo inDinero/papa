675956a4868c8555ee48f7300ef83c838994dd9d
boggs <hello@boggs.xyz>
2017-08-22 17:15:47 +0800

    Add thor

--- lib/ind_flow.rb
+++ lib/ind_flow.rb
@@ -1,5 +1,11 @@
-require "ind_flow/version"
+require 'thor'
+require 'ind_flow/version'
+require 'ind_flow/cli'
+require 'ind_flow/git'
+require 'ind_flow/commander'
+
+require 'pry'
+require 'pry-byebug'
 
 module IndFlow
-  # Your code goes here...
 end
