291496877c0d1c84deec168ba6805d3e6c00ad15
boggs <hello@boggs.xyz>
2017-08-23 18:32:19 +0800

    Add options for release subcommands

--- lib/ind_flow/release.rb
+++ lib/ind_flow/release.rb
@@ -1,37 +1,37 @@
 module IndFlow
   class Release < Thor
-    desc 'start', 'foo'
+    desc 'start', 'Start a new release branch'
+    option :version, aliases: '-v', required: true
     def start
-      version = '1.0.0'
-
+      version = options[:version]
       require 'ind_flow/release/start'
       Release::Start.new(version: version).run
     end
 
-    desc 'add', 'foo'
+    desc 'add', 'Add feature branches to a release branch'
+    option :version, aliases: '-v', required: true
+    option :feature_branches, aliases: '-b', type: :array, required: true
     def add
-      version = '1.0.0'
-      feature_branches = [
-        'feature/1-add-foobars',
-        'feature/2-make-foobars',
-        'feature/3-eat-foobars'
-      ]
+      version = options[:version]
+      feature_branches = options[:feature_branches]
 
       require 'ind_flow/release/add'
       Release::Add.new(version: version, feature_branches: feature_branches).run
     end
 
-    desc 'finish', 'foo'
+    desc 'finish', 'Finish a release branch'
+    option :version, aliases: '-v', required: true
     def finish
-      version = '1.0.0'
+      version = options[:version]
 
       require 'ind_flow/release/finish'
       Release::Finish.new(version: version).run
     end
 
-    desc 'merge', 'foo'
+    desc 'merge', 'Merge the release branch to the base branches'
+    option :version, aliases: '-v', required: true
     def merge
-      version = '1.0.0'
+      version = options[:version]
 
       require 'ind_flow/release/merge'
       Release::Merge.new(version: version).run
