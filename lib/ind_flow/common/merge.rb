4f97a319d419dd22bd4e7af19c9c0fb4976c5ce5
boggs <hello@boggs.xyz>
2017-08-24 20:41:05 +0800

    Turn build_branch into an instance variable so patch methods can override the value

--- lib/ind_flow/common/merge.rb
+++ lib/ind_flow/common/merge.rb
@@ -1,13 +1,13 @@
 module IndFlow
   class Common::Merge
     def run
-      build_branch = "#{@build_type}/#{@version}"
+      @build_branch ||= "#{@build_type}/#{@version}"
 
       queue = CommandQueue.new
 
       @base_branches.each do |branch|
         queue.add Git.checkout(branch_name: branch)
-        queue.add Git.merge(branch_name: build_branch)
+        queue.add Git.merge(branch_name: @build_branch)
         queue.add Git.push(remote: 'origin', branch_name: branch)
 
         if @tag_name && branch == 'master'
