f205c18a00a1e78f36c17907e1f82b7bdfa5cd94
boggs <hello@boggs.xyz>
2017-08-25 12:24:38 +0800

    Modify sandbox to use bare remote repo

--- lib/ind_flow/sandbox/generate.rb
+++ lib/ind_flow/sandbox/generate.rb
@@ -1,7 +1,8 @@
 module IndFlow
   class Sandbox::Generate
     def initialize(options = {})
-      @temp_directory = options[:temp_directory] || '/tmp/ind_flow_sandbox'
+      @remote_repository_directory = '/tmp/ind_flow_sandbox_remote_repository'
+      @local_repository_directory = '/tmp/ind_flow_sandbox_local_repository'
       @details = [
         {
           commit: 'APP-1 - Add butterfree gem',
@@ -32,51 +33,58 @@ module IndFlow
     end
 
     def run
-      @repo_directory = File.expand_path(File.dirname(__dir__))
-      @branches_directory = File.join @repo_directory, 'sandbox', 'branches'
-      create_temp_directory
-      initialize_git_repository
-      add_remote
-      initialize_master_and_develop
-      initialize_branches
+      @project_directory = File.expand_path(File.dirname(__dir__))
+      @branches_directory = File.join @project_directory, 'sandbox', 'branches'
+      setup_remote_repository
+      setup_local_repository
     end
 
     private
-
-    def create_temp_directory
-      `rm -rf #{@temp_directory}`
-      Dir.mkdir @temp_directory
-    end
 
     def gemfile_path(branch)
       File.join @branches_directory, branch, 'Gemfile'
     end
 
     def temp_gemfile_path
-      File.join @temp_directory, 'Gemfile'
+      File.join @local_repository_directory, 'Gemfile'
     end
 
-    def initialize_git_repository
-      Dir.chdir @temp_directory
-      `git init`
+    def setup_remote_repository
+      create_remote_repository_directory
+      initialize_remote_repository
     end
 
-    def add_remote
-      puts "Enter the remote URL for your GitHub repository:"
-      # TODO: Put back in
-      # origin = STDIN.gets.chomp
-      origin = "git@github.com:b-ggs/ind_flow_sandbox.git"
-      `git remote add origin #{origin}`
+    def setup_local_repository
+      clone_remote_repository
+      initialize_master_and_develop
+      initialize_branches
+      cleanup
+    end
+
+    def create_remote_repository_directory
+      `rm -rf #{@remote_repository_directory}`
+      Dir.mkdir @remote_repository_directory
+    end
+
+    def initialize_remote_repository
+      Dir.chdir @remote_repository_directory
+      `git init --bare`
+    end
+
+    def clone_remote_repository
+      `rm -rf #{@local_repository_directory}`
+      Dir.mkdir @local_repository_directory
+      `git clone #{@remote_repository_directory} #{@local_repository_directory}`
+      Dir.chdir @local_repository_directory
     end
 
     def initialize_master_and_develop
-      `cp #{gemfile_path('master')} #{@temp_directory}`
-      `git init`
+      `cp #{gemfile_path('master')} #{@local_repository_directory}`
       `git add .`
       `git commit -m "Initial commit"`
-      `git push origin master --force`
+      `git push origin master`
       `git checkout -b develop`
-      `git push origin develop --force`
+      `git push origin develop`
     end
 
     def initialize_branches
@@ -88,10 +96,17 @@ module IndFlow
         `git checkout #{base_branch}`
         `git checkout -b #{branch}`
         `rm #{temp_gemfile_path}`
-        `cp #{gemfile_path(branch)} #{@temp_directory}`
+        `cp #{gemfile_path(branch)} #{@local_repository_directory}`
         `git add .`
         `git commit -m "#{commit}"`
         `git push origin #{branch} --force`
+      end
+    end
+
+    def cleanup
+      `git checkout develop`
+      @details.each do |detail|
+        `git branch -D #{detail[:branch]}`
       end
     end
   end
