407445248190a294efdcf570a2e26d93afddb23f
boggs <hello@boggs.xyz>
2017-08-30 12:02:56 +0800

    Also tag hotfix finish

--- lib/papa/hotfix/finish.rb
+++ lib/papa/hotfix/finish.rb
@@ -4,6 +4,7 @@ module Papa
       @build_type = 'hotfix'
       @version = version
       additional_branches ||= []
+      @tag_name = version
       @base_branches = ['develop', 'master'] + additional_branches
     end
   end
