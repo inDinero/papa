a2fd4c85ad886bf1e9d8ec0ede5540b29dd6f89f
boggs <hello@boggs.xyz>
2017-12-06 15:28:19 +0800

    Change integration start to just create a new integration build branch

--- lib/papa/cli/integration.rb
+++ lib/papa/cli/integration.rb
@@ -1,17 +1,14 @@
 module Papa
   module CLI
     class Integration < Thor
-      desc 'start', 'Start an integration branch and deploy it to Larga'
+      desc 'start', 'Start an integration branch'
       option :base_branch, aliases: '-b', required: true
-      option :hostname, aliases: '-h'
-      option :skip_larga
       def start
         base_branch = options[:base_branch]
         hostname = options[:hostname]
-        skip_larga = options.has_key?('skip_larga')
 
         require 'papa/task/integration/start'
-        Task::Integration::Start.new(base_branch: base_branch, hostname: hostname, skip_larga: skip_larga).run
+        Task::Integration::Start.new(base_branch: base_branch).run
       end
     end
   end
--- lib/papa/task/integration/start.rb
+++ lib/papa/task/integration/start.rb
@@ -7,47 +7,16 @@ module Papa
   module Task
     module Integration
       class Start < Common::Start
-        def initialize(base_branch:, hostname:, skip_larga:)
+        def initialize(base_branch:)
           @build_type = 'integration'
           @base_branch = base_branch
-          @hostname = hostname
-          @skip_larga = skip_larga
           @build_branch = generate_integration_branch_name
-        end
-
-        def run
-          super
-          return if @skip_larga
-          deploy_options = {
-            branch: @build_branch,
-            hostname: @hostname
-          }
-          queue = [
-            Command::Larga::Type.new,
-            Command::Larga::Deploy.new(deploy_options)
-          ]
-          runner = Runner.new(queue)
-
-          if runner.run
-            success_message_integration
-          else
-            failure_message_integration
-            exit 1
-          end
         end
 
         private
 
         def generate_integration_branch_name
           "integration/#{DateTime.now.strftime('%y.%m.%d.%H.%M.%S').gsub('.0', '.')}"
-        end
-
-        def success_message_integration
-          Helper::Output.success 'Successfully deployed larga instance.'
-        end
-
-        def failure_message_integration
-          Helper::Output.failure 'There was a problem deploying larga instance.'
         end
       end
     end
